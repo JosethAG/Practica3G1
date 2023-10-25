191bb90663d7008a83bf6aeae6c7ef75d812bb64
JosethAG <93845320+JosethAG@users.noreply.github.com>
2023-10-24 23:09:28 -0600

    Integración de clases

--- src/main/java/com/practicaTres/version1/service/FirebaseStorageService.java
+++ src/main/java/com/practicaTres/version1/service/FirebaseStorageService.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.practicaTres.version1.service;
 
-/**
- *
- * @author araya
- */
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
 public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com#
+    final String BucketName = "arboles-c55fe";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "Arboles";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
     
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-8230598465-firebase-adminsdk-ifl36-8b2e4d94f8.json";
 }
