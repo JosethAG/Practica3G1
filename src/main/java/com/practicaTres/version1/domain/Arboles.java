0276d5733a95fdad53ea61f1e74f9ce33dea34d4
JavierMadrigal22 <84678572+JavierMadrigal22@users.noreply.github.com>
2023-10-25 08:11:16 -0600

    Push del controlador, servicios, dominio, FireBasem y DAO

--- src/main/java/com/practicaTres/version1/domain/Arboles.java
+++ src/main/java/com/practicaTres/version1/domain/Arboles.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.practicaTres.version1.domain;
 
-/**
- *
- * @author araya
- */
-public class Arboles {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="arboles")
+public class Arboles implements Serializable{//Serealizacion funciona para almacenar ciertos datos en la maquina
     
+    private static final long serialVersionUID = 1L;//para poder hacer el ciclo de la sumatoria del idCategoria
+    @Id //tabla categoria tiene una llave o identificador que va a ser el ID
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Los valores generados que tipo de estrategia utilizan, identico = sea igual en la BD y en la clase
+    @Column(name="id_arboles")//decir cual es el nombre de la columna de la base de datos  
+    private long idArboles;
+    private String descripcion;
+    private String rutaImagen;
+    private boolean activo;
+
+    public Arboles() {
+    }
+
+    public Arboles(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+    
+   
 }
